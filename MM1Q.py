0cc9221ce5766b65773c1cbf8ee4ef3f259b83ad
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2024-09-17 18:23:16 +0200

    Refactor MM1Q.py to run as Jupyter Notebook (but still as .py file)

--- MM1Q.py
+++ MM1Q.py
@@ -1,90 +1,82 @@
-__metaclass__ = type
+"""
+λ is arrival_rate
+μ is service_rate
+
+https://en.wikipedia.org/wiki/M/M/1_queue
+"""
+
+# %%
 import random
 import csv
+from dataclasses import dataclass
+from statistics import mean
 
 
-# define a class called 'Customer'
+@dataclass
 class Customer:
-    def __init__(self, arrival_date, service_start_date, service_time):
-        self.arrival_date = arrival_date
-        self.service_start_date = service_start_date
-        self.service_time = service_time
-        self.service_end_date = self.service_start_date + self.service_time
-        self.wait = self.service_start_date - self.arrival_date
+    """define a class called 'Customer'"""
+
+    arrival_date: float
+    service_start_date: float
+    service_time: float
+
+    @property
+    def service_end_date(self) -> float:
+        return self.service_start_date + self.service_time
+
+    @property
+    def wait(self) -> float:
+        return self.service_start_date - self.arrival_date
 
 
-# a simple function to sample from negative exponential
-def neg_exp(lambd) -> float:
-    return random.expovariate(lambd)
+def neg_exp(λ: int) -> float:
+    """a simple function to sample from negative exponential."""
+    return random.expovariate(λ)
 
 
-def QSim(lambd=False, mu=False, simulation_time=False):
-    """
-    This is the main function to call to simulate an MM1 queue.
-    """
-
-    # If parameters are not input prompt
-    if not lambd:
-        lambd = eval(input("Inter arrival rate: "))
-    if not mu:
-        mu = eval(input("Service rate: "))
-    if not simulation_time:
-        simulation_time = eval(input("Total simulation time: "))
-
-    # Initialise clock
-    t = 0
-
-    # Initialise empty list to hold all data
-    Customers: list[Customer] = []
-
-    # ----------------------------------
-    # The actual simulation happens here:
-    while t < simulation_time:
-        # calculate arrival date and service time for new customer
-        if len(Customers) == 0:
-            arrival_date = neg_exp(lambd)
-            service_start_date = arrival_date
-        else:
-            arrival_date += neg_exp(lambd)
-            service_start_date = max(arrival_date, Customers[-1].service_end_date)
-        service_time = neg_exp(mu)
-
-        # create new customer
-        Customers.append(Customer(arrival_date, service_start_date, service_time))
-
-        # increment clock till next end of service
-        t = arrival_date
-    # ----------------------------------
-
+def print_stats(customers: list[Customer], tick: float) -> None:
     # calculate summary statistics
-    Waits = [a.wait for a in Customers]
-    Mean_Wait = sum(Waits) / len(Waits)
+    waits: list[float] = []
+    total_times: list[float] = []
+    service_times: list[float] = []
 
-    Total_Times = [a.wait + a.service_time for a in Customers]
-    Mean_Time = sum(Total_Times) / len(Total_Times)
+    for c in customers:
+        waits.append(c.wait)
+        total_times.append(c.wait + c.service_time)
+        service_times.append(c.service_time)
 
-    Service_Times = [a.service_time for a in Customers]
-    Mean_Service_Time = sum(Service_Times) / len(Service_Times)
+    # Compute means and utilization
+    mean_wait = mean(waits)
+    mean_time = mean(total_times)
+    mean_service_time = mean(service_times)
 
-    Utilisation = sum(Service_Times) / t
+    utilisation = sum(service_times) / tick
 
     # output summary statistics to screen
     print("")
     print("Summary results:")
     print("")
-    print("Number of customers: ", len(Customers))
-    print("Mean Service Time: ", Mean_Service_Time)
-    print("Mean Wait: ", Mean_Wait)
-    print("Mean Time in System: ", Mean_Time)
-    print("Utilisation: ", Utilisation)
+    print(f"Number of customers: {len(customers)}")
+    print(f"Mean Service Time: {mean_service_time:.2f}")
+    print(f"Mean Wait: {mean_wait:.2f}")
+    print(f"Mean Time in System: {mean_time:.2f}")
+    print(f"Utilisation: {utilisation:.2f}")
+
     print("")
 
-    # prompt user to output full data set to csv
-    if eval(input("Output data to csv (True/False)? ")):
-        outfile = open(
-            "MM1Q-output-(%s,%s,%s).csv" % (lambd, mu, simulation_time), "wb"
-        )
+
+def save_to_csv(
+    customers: list[Customer],
+    arrival_rate: int,
+    service_rate: int,
+    simulation_time: int,
+) -> None:
+    with open(
+        file=f"MM1Q-output-({arrival_rate},{service_rate},{simulation_time}).csv",
+        mode="wb",
+    ) as outfile:
         output = csv.writer(outfile)
+        # write the header
         output.writerow(
             [
                 "Customer",
@@ -95,17 +87,95 @@ def QSim(lambd=False, mu=False, simulation_time=False):
                 "Service_End_Date",
             ]
         )
-        i = 0
-        for customer in Customers:
-            i = i + 1
-            outrow = []
-            outrow.append(i)
-            outrow.append(customer.arrival_date)
-            outrow.append(customer.wait)
-            outrow.append(customer.service_start_date)
-            outrow.append(customer.service_time)
-            outrow.append(customer.service_end_date)
-            output.writerow(outrow)
-        outfile.close()
-    print("")
-    return
+        # write the data
+        for i, c in enumerate(customers, start=0):
+            output.writerow(
+                [
+                    i,
+                    c.arrival_date,
+                    c.wait,
+                    c.service_start_date,
+                    c.service_time,
+                    c.service_end_date,
+                ]
+            )
+
+
+def simulation(
+    simulation_time: int, arrival_rate: int, service_rate: int
+) -> tuple[list[Customer], float]:
+    """Run the simulation and return the generated customers and the end time of the simulation"""
+    # Initialise clock
+    tick = 0
+
+    # Initialise empty list to hold all data
+    customers: list[Customer] = []
+
+    # ----------------------------------
+    # The actual simulation happens here:
+    while tick < simulation_time:
+        # calculate arrival date and service time for new customer
+        arrival_date = 0
+        if len(customers) == 0:
+            arrival_date = neg_exp(arrival_rate)
+            service_start_date = arrival_date
+        else:
+            arrival_date += neg_exp(arrival_rate)
+            service_start_date = max(arrival_date, customers[-1].service_end_date)
+        service_time = neg_exp(service_rate)
+
+        # create new customer
+        customers.append(Customer(arrival_date, service_start_date, service_time))
+
+        # increment clock till next end of service
+        tick = arrival_date
+    # ----------------------------------
+    return customers, tick
+
+
+def QSim(
+    arrival_rate: int | None = None,
+    service_rate: int | None = None,
+    simulation_time: int | None = None,
+    output_file: bool | None = None,
+) -> None:
+    """
+    This is the main function to call to simulate an MM1 queue.
+
+    arrival_rate = λ
+    service_rate = μ
+
+    https://en.wikipedia.org/wiki/M/M/1_queue
+    """
+
+    # If parameters are not input prompt
+    if arrival_rate is None:
+        arrival_rate = int(input("Inter arrival rate: ")) or 1
+    if service_rate is None:
+        service_rate = int(input("Service rate: ")) or 2
+    if simulation_time is None:
+        simulation_time = int(input("Total simulation time: ")) or 10
+    if output_file is None:
+        output_file = bool(input("Output data to csv (True/False)? ")) or False
+
+    # type-check parameters, so we don't get type-hinting complaints
+    assert isinstance(arrival_rate, int)
+    assert isinstance(service_rate, int)
+    assert isinstance(simulation_time, int)
+    assert isinstance(output_file, bool)
+
+    customers, tick = simulation(simulation_time, arrival_rate, service_rate)
+
+    print_stats(customers, tick)
+
+    if output_file:
+        save_to_csv(customers, arrival_rate, service_rate, simulation_time)
+
+
+# %%
+# run the simulation with some sane defaults
+QSim(arrival_rate=1, service_rate=2, simulation_time=10, output_file=False)
+
+# %%
+# run the simulation and ask the user for parameters
+QSim()
